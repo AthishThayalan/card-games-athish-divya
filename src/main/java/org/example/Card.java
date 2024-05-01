2c9d604177eea0d43b2d1662a056e69a2b7bed88
Athish Thayalan <tathish@hotmail.co.uk>
2024-05-01 16:24:19 +0100

    Finished Card constructor.

--- src/main/java/org/example/Card.java
+++ src/main/java/org/example/Card.java
@@ -7,8 +7,48 @@ public class Card {
 
     public Card(Suits suit,char symbol){
         this.suit = suit;
+        this.symbol = symbol;
+        this.value = getValueFromSymbol(symbol);
+    }
+
+    public Suits getSuit() {
+        return suit;
+    }
+
+    public char getSymbol() {
+        return symbol;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getValueFromSymbol(char symbol){
+        if(Character.isDigit(symbol)){
+            return Integer.parseInt(String.valueOf(symbol));
+        } else {
+            switch(symbol){
+                case'J':
+                    return 11;
+                case'Q':
+                    return 12;
+                case'K':
+                    return 13;
+                case'A':
+                    return 14;
+                default:
+                    return -1;
+            }
+        }
+
 
     }
+
+    public void printCardDetails(){
+        System.out.println("Suit: "+this.suit+", symbol: "+this.symbol+", value: "+this.value);
+    }
+
+
 
 
 }
